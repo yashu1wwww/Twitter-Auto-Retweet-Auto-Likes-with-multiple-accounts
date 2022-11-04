d9f9f07a119694b2fc2b1c8942db28b85ebcc1d2
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-11-04 12:23:56 +0530

    Update rl.py

--- rl.py
+++ rl.py
@@ -40,12 +40,37 @@ time.sleep(2)
 
 driver.close()
 
-import time
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+driver= webdriver.Chrome()
+driver.maximize_window()
+
+driver.get("https://twitter.com/i/flow/login")
+time.sleep(10)
+email = driver.find_element_by_name('text')
+email.send_keys("@twitter123") #replace with your twitter acc and do it for below code also 
+email.send_keys(Keys.ENTER)
+time.sleep(3)
+password = driver.find_element_by_name("password")
+password.send_keys("twiee1@#$") #replace with your twitter password and dot it for below code also
+password.send_keys(Keys.ENTER)
+time.sleep(5)
+
+with open("urls.txt") as f:
+    for url in f:
+        driver.get(url) 
+
+time.sleep(5)
+
+retweet_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Retweet']"))).click()
+
+retweet_confirm = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@data-testid='retweetConfirm']"))).click()
+
+time.sleep(2)
+
+like_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Like']"))).click()
+
+time.sleep(2)
+
+driver.close()
 
 driver= webdriver.Chrome()
 driver.maximize_window()
@@ -79,13 +104,6 @@ time.sleep(2)
 
 driver.close()
 
-import time
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 driver= webdriver.Chrome()
 driver.maximize_window()
 
@@ -117,52 +135,6 @@ like_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By
 time.sleep(2)
 
 driver.close()
-
-import time
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-driver= webdriver.Chrome()
-driver.maximize_window()
-
-driver.get("https://twitter.com/i/flow/login")
-time.sleep(10)
-email = driver.find_element_by_name('text')
-email.send_keys("@twitter123") #replace with your twitter acc and do it for below code also 
-email.send_keys(Keys.ENTER)
-time.sleep(3)
-password = driver.find_element_by_name("password")
-password.send_keys("twiee1@#$") #replace with your twitter password and dot it for below code also
-password.send_keys(Keys.ENTER)
-time.sleep(5)
-
-with open("urls.txt") as f:
-    for url in f:
-        driver.get(url) 
-
-time.sleep(5)
-
-retweet_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Retweet']"))).click()
-
-retweet_confirm = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@data-testid='retweetConfirm']"))).click()
-
-time.sleep(2)
-
-like_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Like']"))).click()
-
-time.sleep(2)
-
-driver.close()
-
-import time
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 driver= webdriver.Chrome()
 driver.maximize_window()
@@ -195,3 +167,4 @@ like_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By
 time.sleep(2)
 
 driver.close()
+
